f47cb4fdabee9dbb31e45e172a0d144156dee5d5
maxierra <maxi.erramouspe77@gmail.com>
2025-03-15 13:33:00 -0300

    Updated reset password page to handle prerendering issues

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -2,8 +2,19 @@
 
 import AuthLayout from '../../components/auth/AuthLayout';
 import ResetPasswordForm from '../../components/auth/ResetPasswordForm';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function ResetPassword() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirigir si no se está en el cliente
+    if (typeof window === 'undefined') {
+      router.push('/'); // Redirigir a la página de inicio o a otra página adecuada
+    }
+  }, [router]);
+
   return (
     <AuthLayout title="Recupera tu contraseña">
       <ResetPasswordForm />
